fbb9c0b14045bac2dd1ae70f0dfb46fbd369f3aa
nmr0079 <92203516+nmr0079@users.noreply.github.com>
2022-08-24 14:04:14 +0530

    Time: 27 ms (73.41%), Space: 5.9 MB (72.69%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n == 0)
-            return false;
-        if(n % 3 == 0)
-            return isPowerOfThree(n/3);
+      if(n == 0)
+          return false;
+        
+        while(n % 3 == 0)
+            n/=3;
         if(n == 1)
             return true;
         return false;
-    
         }
 };
