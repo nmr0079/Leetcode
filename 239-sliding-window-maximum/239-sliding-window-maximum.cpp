0c2b75c99334185e2dd9b294bfd94e520cf749d5
nmr0079 <92203516+nmr0079@users.noreply.github.com>
2022-08-28 11:44:15 +0530

    Time: 398 ms (73.51%), Space: 136.6 MB (38.86%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    deque<int> dq;
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-          deque<int> dq;
-          vector<int> result;
-          for(int i = 0;i < nums.size();i++){
-              while(!dq.empty() and nums[dq.back()] < nums[i])
-                  dq.pop_back();
-              dq.push_back(i);
-              if(i >= k-1)
-              {
-                  result.push_back(nums[dq.front()]);
-                  if(dq.front() == i - k+1)
-                        dq.pop_front();
-              }   
-          }
-          return result;
+       int n = nums.size();
+        vector<int> left(n), right(n);
+        int wind_size = n - k + 1;
+        vector<int> output(wind_size);
         
+        for(int i = 0;i < n;i++){
+            left[i] = nums[i];
+            if(i % k != 0)
+                left[i] = max(left[i], left[i-1]);
+            
+        }
+        
+        right[n-1] = nums[n- 1];
+        for(int j = n-2;j >= 0;j--){
+            right[j] = nums[j];
+            if(j % k != k - 1)
+                right[j] = max(right[j], right[j+1]);
+        }
+        
+        for(int i = 0;i < wind_size;i++){
+            output[i] = max(right[i], left[i+k-1]);
+        }
+        return output;
     }
 };
