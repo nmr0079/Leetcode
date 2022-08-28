9f6ab9888913e6d84827efeaeb6d9dd6078d0454
nmr0079 <92203516+nmr0079@users.noreply.github.com>
2022-08-28 11:28:28 +0530

    Time: 475 ms (53.07%), Space: 134.8 MB (62.16%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -5,13 +5,13 @@ public:
           deque<int> dq;
           vector<int> result;
           for(int i = 0;i < nums.size();i++){
-              while(!dq.empty() and dq.back() < nums[i])
+              while(!dq.empty() and nums[dq.back()] < nums[i])
                   dq.pop_back();
-              dq.push_back(nums[i]);
+              dq.push_back(i);
               if(i >= k-1)
               {
-                  result.push_back(dq.front());
-                  if(dq.front() == nums[i - k+1])
+                  result.push_back(nums[dq.front()]);
+                  if(dq.front() == i - k+1)
                         dq.pop_front();
               }   
           }
