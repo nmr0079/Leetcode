1a26a064224770967cf39403a365d2f6f251ff3a
nmr0079 <92203516+nmr0079@users.noreply.github.com>
2022-10-12 10:10:08 +0530

    Time: 4 ms (83.92%), Space: 8.8 MB (50.14%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -4,20 +4,14 @@ public:
         int n = nums.size();
         if(n == 1)
             return 0;
-        for(int i = 0;i < n;i++){
-            if(i == 0){
-                if(nums[i] > nums[i+1])
-                    return 0;
-            }
-            else if(i == n-1){
-                if(nums[i] > nums[i-1])
-                    return n-1;
-            }
-            else{
-                   if(nums[i-1] < nums[i] && nums[i+1] < nums[i])
-                       return i;
-            }
+        int left = 0, right = n-1;
+        while(left < right){
+            int mid = left+(right - left)/2;
+            if(nums[mid] < nums[mid+1])
+                left = mid+1;
+            else
+                right = mid;
         }
-        return -1;
+        return left;
     }
 };
