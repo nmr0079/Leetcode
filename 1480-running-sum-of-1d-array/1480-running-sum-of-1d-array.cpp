21895dda3620bbb49bf762cfbf83119c0f6715ee
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-02-01 10:37:07 +0530

    Time: 4 ms (63.91%), Space: 8.6 MB (43.48%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -3,15 +3,13 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         int n = nums.size();
         int prefixsum;
-        vector<int> runningsum;
         prefixsum = nums[0];
-        runningsum.push_back(prefixsum);
         for(int i = 1;i < n;i++){
             int currsum = prefixsum + nums[i];
-            runningsum.push_back(currsum);
+            nums[i] = currsum;
             prefixsum = currsum;
         }
         
-        return runningsum;
+        return nums;
     }
 };
