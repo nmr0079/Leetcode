f2a3bdc7ba39098ae675835f14b558ff41a8d24b
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-02-02 12:27:49 +0530

    Time: 3 ms (95.35%), Space: 7 MB (94.64%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,70 +1,39 @@
 class Solution {
 public:
-    bool isSafeforQ(int row,int col,vector<string> &board,int n){
-        int temprow = row; //Forholding the original argument values
-        int tempcol = col;
-        
-        //For checking the upper diagonal
-        while(row>=0 && col>=0){
-            if(board[row][col] == 'Q'){
-                return false;
-            }
-            
-            row--;
-            col--;
-        }
-        
-        row = temprow;
-        col = tempcol;
-        
-        //For checking the lower diagonal direction
-        while(row < n && col >= 0){
-            if(board[row][col] == 'Q'){
-                return false;
-            }
-            row++;
-            col--;
-        }
-        
-        row = temprow;
-        col = tempcol;
-        
-        //For the left direction
-        while(col >= 0){
-            if(board[row][col] == 'Q'){
-                return false;
-            }
-            col--;
-        }
-        
-        return true;
-    }
-    
-    //For solving each column's case,where the queen has to be placed
-    void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
+     //For solving each column's case,where the queen has to be placed
+    void solve(int col,vector<string> &board,vector<vector<string>> &ans,vector<int> &leftRow,vector<int> &upperDiagonal,vector<int> &lowerDiagonal,int n){
         if(col == n){  //if all the columns has been filled with a queen
             ans.push_back(board);
             return;
         }
         
         for(int row = 0;row < n;row++){
-            if(isSafeforQ(row,col,board,n)){
+            if(leftRow[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1+col-row] == 0){
                 board[row][col] = 'Q';
-                solve(col+1,board,ans,n); //Do a dfs recursively,for this choice of row,col
+                leftRow[row] = 1; //Hash value for the left direction updated to 1(occupied)
+                lowerDiagonal[row+col] =1;
+                upperDiagonal[n-1+col-row] = 1;
+                solve(col+1,board,ans,leftRow,upperDiagonal,lowerDiagonal,n);
                 
-                //backtracking step
-                board[row][col] = '.';  //After returning from the col+1 call,remove Q from that pos
+                //backtracking steps
+                board[row][col] = '.';
+                leftRow[row] = 0; //Hash value for the left direction updated to 0(not occupied)
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1+col-row] = 0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
+        vector<int> leftRow(n, 0); //hash map for the left direction
+        vector<int> lowerDiagonal(2*n-1, 0);
+        vector<int> upperDiagonal(2*n-1, 0);
         string s(n, '.');
         for(int row = 0;row < n;row++){
             board[row] = s;
         }
-        solve(0, board, ans, n);
+        solve(0, board, ans, leftRow, upperDiagonal, lowerDiagonal, n);
         return ans;
     }
 };
