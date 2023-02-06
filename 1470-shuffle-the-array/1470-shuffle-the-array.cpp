aa1ccfff7e9ada07fc7f6b966950f9a1a509c07a
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-02-06 21:42:05 +0530

    Time: 11 ms (28.61%), Space: 9.8 MB (61.93%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> shuffled_array(2*n);
-        int ptr1,ptr2;
-        ptr1 = 0;
-        ptr2 = n;
-        int i = 0;
-        while(ptr1 < n && ptr2 < 2*n )
-        {
-            shuffled_array[i] = nums[ptr1];
-            shuffled_array[++i] = nums[ptr2];
-            ++ptr1;
-            ++ptr2;
-            ++i;
-        }
-        return shuffled_array;
-    }
+    //Solution with TC - O(n) and SC - O(n)
+//     vector<int> shuffle(vector<int>& nums, int n) {
+//         vector<int> shuffled_array(2*n);
+//         int ptr1,ptr2;
+//         ptr1 = 0;
+//         ptr2 = n;
+//         int i = 0;
+//         while(ptr1 < n && ptr2 < 2*n )
+//         {
+//             shuffled_array[i] = nums[ptr1];
+//             shuffled_array[++i] = nums[ptr2];
+//             ++ptr1;
+//             ++ptr2;
+//             ++i;
+//         }
+//         return shuffled_array;
+//     }
+    
+      //A solution with TC - O(n) and SC - O(1)
+      vector<int> shuffle(vector<int>& nums, int n) {
+          //store both the number pairs to the right side of the array
+          for(int i = n;i < nums.size();i++){
+              nums[i] = (nums[i] << 10) + nums[i - n]; //The max of two no: will be 1000(from the constraint)
+                                                       //take a number greater than the max,here 1024 (2^10)
+          }
+          
+          int idx = 0;
+          //Now retrieve the numbers and place them on the right index
+          for(int i = n;i < nums.size();i++,idx+=2){
+              nums[idx] = nums[i] & 1023;  //== nums[i] % 1024 (remainder)
+              nums[idx+1] = nums[i] >> 10; //== nums[i]/1024 (quotient)
+          }
+          return nums;
+      }
 };
