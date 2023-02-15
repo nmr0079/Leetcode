0dd140e5b3e09aaf6dfe0cb3a4d8aaa5712543c4
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-02-15 10:06:57 +0530

    Time: 46 ms (19.50%), Space: 27.2 MB (95.67%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
@@ -2,46 +2,18 @@ class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
         int n = num.size();
-        vector<int> karray;
         
-        
+        //carrying k itself as the carry
+        for(int i = n-1;i >= 0 && k >= 0;i--){
+            num[i] += k;
+            k = num[i]/10;
+            num[i] = num[i]%10;
+        }
         
         while(k > 0){
-            karray.insert(karray.begin(),k%10);
-            k=k/10;
+           num.insert(num.begin(),k%10);
+           k/=10;
         }
-        
-        int ksize = karray.size();
-        
-        if(n > ksize){
-            while(ksize < n){
-                karray.insert(karray.begin(),0);
-                ksize++;
-            }
-        }
-        else if(n < ksize){
-            while(n < ksize){
-                num.insert(num.begin(),0);
-                n++;
-            }
-        }
-        
-        int carry = 0;
-        for(int i = n-1;i>=0;i--){
-            int c = num[i]+karray[i]+carry;
-            if(c > 9){
-                num[i] = c%10;
-                carry = 1;
-            }
-            else{
-                num[i] = c;
-                carry = 0;
-            }
-        }
-        
-        if(carry)
-            num.insert(num.begin(),carry);
-        
         return num;
     }
 };
