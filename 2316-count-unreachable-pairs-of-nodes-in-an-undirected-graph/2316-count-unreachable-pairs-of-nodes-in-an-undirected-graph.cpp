e82757e2db87db3aac112ba3e1dc3e06d73ca90a
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-03-25 14:57:15 +0530

    Time: 522 ms (94.06%), Space: 184.3 MB (60.83%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,66 +1,39 @@
-class Disjoint_Set {
-    vector<int> rank,parent;  //two arrays ro be managed
-    
-    public:
-        Disjoint_Set(int n){
-            rank.resize(n+1,0);  //n+1 as it will work for both 1 indexed and 0-indexed
-                                 //initially everyone's rank will be 0
-            parent.resize(n+1);
-            for(int i = 0;i <= n;i++)
-                parent[i] = i;           //initially every node will be a single component
-        }
-    
-        int findParent(int node){
-            if(node == parent[node])
-                return node;
-            
-            return parent[node] = findParent(parent[node]);  //path compression step
-        }
-    
-       void unionByRank(int u,int v){
-           int ultp_u = findParent(u);
-           int ultp_v = findParent(v);
-           
-           if(ultp_u == ultp_v)
-               return;
-           
-           if(rank[ultp_u] < rank[ultp_v]){
-               parent[ultp_u] = ultp_v;   //rank of ultp_v doesn't change as ultp_u has lesser rank 
-           }
-           
-           else if(rank[ultp_v] < rank[ultp_u]){
-               parent[ultp_v] = ultp_u;
-           }
-           
-           else{
-               parent[ultp_v] = ultp_u;
-               rank[ultp_u]++;
-           }
-       }
-    
-};
 class Solution {
 public:
+    int dfs(int node,vector<vector<int>>& adj,vector<bool>& visited){
+        visited[node] = true;
+        int count = 1;
+        
+        for(auto& nbhr: adj[node]){
+            if(!visited[nbhr]){
+                count += dfs(nbhr,adj,visited);
+            }
+        }
+        
+        return count;  //For returning the number of nodes in a connected component
+    }
+    
     long long countPairs(int n, vector<vector<int>>& edges) {
-        Disjoint_Set ds(n);
-        int n_edges = edges.size();
+        vector<vector<int>> adj(n);
         
-        for(int i = 0;i < n_edges;i++){
-            ds.unionByRank(edges[i][0],edges[i][1]);
-        }
-        unordered_map<int,int> nodesConnected;
-        for(int i = 0;i < n;i++){
-            nodesConnected[ds.findParent(i)]++;
+        //adjacency list formation
+        for(auto& edge: edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
         
+        vector<bool> visited(n);
+        long long pair_count = 0;
         long long remaining_nodes = n;
-        long long paircount = 0;
         
-        for(auto& ultparent: nodesConnected){
-            int no_nodesConnected = ultparent.second;
-            paircount += no_nodesConnected * (remaining_nodes - no_nodesConnected);
-            remaining_nodes -= no_nodesConnected;
+        for(int i = 0;i < n;i++){
+            if(!visited[i]){
+               int count_nodes = dfs(i,adj,visited);
+               pair_count += count_nodes*(remaining_nodes - count_nodes);
+               remaining_nodes -= count_nodes;
+            }
         }
-        return paircount;
+        
+        return pair_count;
     }
 };
