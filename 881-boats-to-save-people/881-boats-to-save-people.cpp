6c694c3045d70d50195d9362f25aa9bb1dfc2d01
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-04-03 15:29:23 +0530

    Time: 87 ms (66.67%), Space: 42.1 MB (11.29%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-    bool enoughBoats(int mid,vector<int>& people, int limit){
+    int numRescueBoats(vector<int>& people, int limit) {
         int n = people.size();
         int left = 0, right = n-1;
+        sort(people.begin(),people.end());
+        int count = 0;
         while(left <= right){
             if(people[right] == limit){
-                mid--;
+                count++;
                 right--;
             }
             else if(people[left]+people[right] > limit)
             {
-                mid--;
+                count++;
                 right--;
             }
             else{
-                mid--;
+                count++;
                 right--;
                 left++;
             }
          
         }
-        if(mid >= 0)
-            return true;
-        return false;
-    }
-    
-    int numRescueBoats(vector<int>& people, int limit) {
-        int n = people.size();
-        int left = 1, right = n;
-        sort(people.begin(),people.end());
-        while(left < right){
-            int mid = left + (right - left)/2;
-            if(enoughBoats(mid,people,limit))
-                right = mid;
-            else
-                left = mid+1;
-        }
-        return right;
+        return count;
     }
 };
