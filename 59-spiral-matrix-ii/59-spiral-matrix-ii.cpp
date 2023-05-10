a53ef79431a19936ed21b9f04d18c24cb2286d36
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-05-10 09:25:25 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (70.13%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -4,26 +4,29 @@ public:
         vector<vector<int>> matrix(n, vector<int>(n));
         int left = 0, right = n-1, top = 0, bottom = n-1, direction = 1, elem = 1;
         while(left <= right && top <= bottom){
-            if(direction == 1)
+            if(direction == 1) //For the first row in the spiral
             {
                 for(int i = left;i <= right;i++)
                     matrix[top][i] = elem++;
                 top++;
                 direction = 2;
             }
-            else if(direction == 2){
+            else if(direction == 2) //For the last col in the spiral
+            {
                 for(int i = top;i <= bottom;i++)
                     matrix[i][right] = elem++;
                 right--;
                 direction = 3;
             }
-            else if(direction == 3){
+            else if(direction == 3) //For the last row in the spiral
+            {
                 for(int i = right;i >= left;i--)
                     matrix[bottom][i] = elem++;
                 bottom--;
                 direction = 4;
             }
-            else if(direction == 4){
+            else if(direction == 4)  //For the first col in the spiral
+            {
                 for(int i = bottom;i >= top;i--)
                     matrix[i][left] = elem++;
                 left++;
