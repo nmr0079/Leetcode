54ce655a3d7c76d8b5d4a8d59072da3d48b8db52
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-05-12 20:53:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (38.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -16,18 +16,28 @@ public:
         
 //         return prev;
 //     }
-    int distinctWays(int n, vector<int>&dp){
-        if(n == 1 || n == 0)
-             return 1;
-         if(n < 0)
-             return 0;
-        if(dp[n] != -1)
-            return dp[n];
+//     //Memoized Recursive Solution
+//     int distinctWays(int n, vector<int>&dp){
+//         if(n == 1 || n == 0)
+//              return 1;
+//          if(n < 0)
+//              return 0;
+//         if(dp[n] != -1)
+//             return dp[n];
          
-         return dp[n] = distinctWays(n-1, dp) + distinctWays(n-2, dp);
-    }
-     int climbStairs(int n) {
-         vector<int> dp(n+1, -1);
-         return distinctWays(n, dp);
-     }
+//          return dp[n] = distinctWays(n-1, dp) + distinctWays(n-2, dp);
+//     }
+//      int climbStairs(int n) {
+//          vector<int> dp(n+1, -1);
+//          return distinctWays(n, dp);
+//      }
+    //Tabulation/BottomUp/DynamicProg
+    int climbStairs(int n) {
+          vector<int> dp(n+1);
+          dp[0] = dp[1] = 1;
+          for(int i = 2;i <= n;i++){
+              dp[i] = dp[i-1] + dp[i-2];
+          }
+          return dp[n];
+      }
 };
