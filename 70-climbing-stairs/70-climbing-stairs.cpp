fb7225f6da974b23ab6ae53818206a7f214fb1dc
Nithin M R <92203516+nmr0079@users.noreply.github.com>
2023-05-12 20:48:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (38.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n == 0 || n == 1){
-            return 1;
-        }
+//     int climbStairs(int n) {
+//         if(n == 0 || n == 1){
+//             return 1;
+//         }
         
-        int prev2,prev;
-        prev2 = 1;
-        prev = 1;
-        for(int i = 2;i <= n;i++){
-            int curr = prev2 + prev;
-            prev2 = prev;
-            prev = curr;
-        }
+//         int prev2,prev;
+//         prev2 = 1;
+//         prev = 1;
+//         for(int i = 2;i <= n;i++){
+//             int curr = prev2 + prev;
+//             prev2 = prev;
+//             prev = curr;
+//         }
         
-        return prev;
+//         return prev;
+//     }
+    int distinctWays(int n, vector<int>&dp){
+        if(n == 1 || n == 0)
+             return 1;
+         if(n < 0)
+             return 0;
+        if(dp[n] != -1)
+            return dp[n];
+         
+         return dp[n] = distinctWays(n-1, dp) + distinctWays(n-2, dp);
     }
+     int climbStairs(int n) {
+         vector<int> dp(n+1, -1);
+         return distinctWays(n, dp);
+     }
 };
